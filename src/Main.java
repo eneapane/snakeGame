0cece112bffa5e4c753746df3e6d57d7814280e3
eneapane <eneapane1@gmail.com>
2023-01-16 23:10:29 +0100

    Delete code comments in Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,19 +4,10 @@ import java.awt.*;
 public class Main {
     public static void main(String[] args) {
         JFrame frame = new JFrame();
-
         frame.add(MyPanel.getMyPanel());
-
-        //to be edited
-
-        //
-
         frame.addKeyListener(new MyKeyListener(Snake.getSnake()));
-
         frame.setComponentOrientation(ComponentOrientation.LEFT_TO_RIGHT);
-
         frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        //frame.setUndecorated(true);
         frame.setVisible(true);
     }
 }
